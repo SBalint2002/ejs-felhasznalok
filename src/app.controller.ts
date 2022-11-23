f8b99bea5462ad46733d2d42a0c57f605b297de9
Sárándi <sarandi.balint@gmail.com>
2022-11-23 15:14:32 +0100

    tablazat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,12 +4,12 @@ import { User } from './user';
 let felhlista: Array<User> = [];
 
 felhlista.push(
-  new User('Bö','férfi',new Date('2003-01-09'), 60000, true),
-  new User('Gipsz János','nő',new Date('1983-10-12'), 30000, false),
-  new User('Fertő Miklós','férfi',new Date('1953-12-01'), 55000, false),
-  new User('Géza','férfi',new Date('1999-09-19'), 53500, true),
-  new User('Női Név','férfi',new Date('2002-12-29'), 75000, false),
-  new User('Mégegy Nő','férfi',new Date('1976-03-12'), 98000, true),
+  new User('Bö', 'férfi', new Date('2003-01-09'), 60000, true),
+  new User('Gipsz János', 'nő', new Date('1983-10-12'), 30000, false),
+  new User('Fertő Miklós', 'férfi', new Date('1953-12-01'), 55000, false),
+  new User('Géza', 'férfi', new Date('1999-09-19'), 53500, true),
+  new User('Női Név', 'férfi', new Date('2002-12-29'), 75000, false),
+  new User('Mégegy Nő', 'férfi', new Date('1976-03-12'), 98000, true),
 );
 
 @Controller()
@@ -17,6 +17,6 @@ export class AppController {
   @Get()
   @Render('index')
   root() {
-    return { message: 'Hello world!' };
+    return { users: felhlista };
   }
 }
--- src/user.ts
+++ src/user.ts
@@ -5,6 +5,26 @@ export class User {
     balance: number;
     isActive: boolean;
 
+    get nameStr(): string {
+        return this.name;
+    }
+
+    get genderStr(): string {
+        return this.gender;
+    }
+
+    get brithDateStr(): String {
+        return this.birthDate.toLocaleDateString();
+    }
+
+    get balanceNum(): number {
+        return this.balance;
+    }
+
+    get active(): string {
+        return this.isActive ? "Active" : "Inactive";
+    }
+
     constructor(name: string, gender: string, birthDate: Date, balance: number, isActive: boolean) {
         this.name = name;
         this.gender = gender;
